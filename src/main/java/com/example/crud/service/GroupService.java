fde1cfa1d3296fa3853b25110a93472ea62a1c6f
mischelll <mishel.ivanovv@gmail.com>
2022-06-19 20:22:13 +0300

    add crud logic

--- src/main/java/com/example/crud/service/GroupService.java
+++ src/main/java/com/example/crud/service/GroupService.java
@@ -4,7 +4,7 @@ import com.example.crud.service.dto.GroupDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-@Service
+
 public interface GroupService {
 
     void delete(Long id);
--- src/main/java/com/example/crud/service/impl/GroupServiceImpl.java
+++ src/main/java/com/example/crud/service/impl/GroupServiceImpl.java
@@ -1,19 +1,41 @@
 package com.example.crud.service.impl;
 
+import com.example.crud.domain.Group;
+import com.example.crud.mapper.GroupMapper;
+import com.example.crud.repository.GroupRepository;
 import com.example.crud.service.GroupService;
 import com.example.crud.service.dto.GroupDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class GroupServiceImpl implements GroupService {
-    @Override
-    public void delete(Long id) {
 
+    private final Logger log = LoggerFactory.getLogger(GroupServiceImpl.class);
+
+    private final GroupRepository groupRepository;
+
+    private final GroupMapper groupMapper;
+
+    public GroupServiceImpl(GroupRepository groupRepository, GroupMapper groupMapper) {
+        this.groupRepository = groupRepository;
+        this.groupMapper = groupMapper;
     }
 
     @Override
-    public GroupDTO save(GroupDTO eventDTO) {
-        return null;
+    public void delete(Long id) {
+        log.info("Delete Group by id: {}", id);
+        groupRepository.deleteById(id);
+    }
+
+    @Override
+    public GroupDTO save(GroupDTO groupDTO) {
+        log.info("Create Group: {}", groupDTO);
+        Group entity = groupMapper.toEntity(groupDTO);
+        Group group = groupRepository.save(entity);
+        return groupMapper.toDto(group);
     }
 
     @Override
