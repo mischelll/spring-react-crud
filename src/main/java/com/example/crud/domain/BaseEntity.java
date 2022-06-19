60da657d31b7414210eda460baf1a49c9c19cdce
mischelll <mishel.ivanovv@gmail.com>
2022-06-19 20:21:37 +0300

    create sequence generator

--- src/main/java/com/example/crud/domain/BaseEntity.java
+++ src/main/java/com/example/crud/domain/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.example.crud.domain;
 
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
@@ -8,7 +9,7 @@ import javax.persistence.MappedSuperclass;
 public abstract class BaseEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
     private Long id;
 
     public Long getId() {
