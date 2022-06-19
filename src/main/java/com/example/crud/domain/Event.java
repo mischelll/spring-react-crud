2e631e9bc1a5bbb14b7dee85adda636e4c475a72
mischelll <mishel.ivanovv@gmail.com>
2022-06-19 20:21:59 +0300

    remove lombok

--- src/main/java/com/example/crud/domain/Event.java
+++ src/main/java/com/example/crud/domain/Event.java
@@ -1,25 +1,80 @@
 package com.example.crud.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import java.time.Instant;
+import java.util.Objects;
 import java.util.Set;
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
 @Entity
 @Table(name = "event")
 public class Event extends BaseEntity{
+    @Column(name = "date")
     private Instant date;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "description")
     private String description;
+
     @ManyToMany
     private Set<User> attendees;
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<User> getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(Set<User> attendees) {
+        this.attendees = attendees;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return Objects.equals(date, event.date) && Objects.equals(title, event.title) && Objects.equals(description, event.description) && Objects.equals(attendees, event.attendees);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, title, description, attendees);
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "date=" + date +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", attendees=" + attendees +
+                '}';
+    }
 }
--- src/main/java/com/example/crud/service/dto/GroupDTO.java
+++ src/main/java/com/example/crud/service/dto/GroupDTO.java
@@ -1,4 +1,63 @@
 package com.example.crud.service.dto;
 
+import javax.validation.constraints.NotNull;
+
+
 public class GroupDTO {
+
+    @NotNull
+    private String name;
+    private String address;
+    private String city;
+    private String stateOfProvince;
+    private String country;
+    private String postalCode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStateOfProvince() {
+        return stateOfProvince;
+    }
+
+    public void setStateOfProvince(String stateOfProvince) {
+        this.stateOfProvince = stateOfProvince;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 }
