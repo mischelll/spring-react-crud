0429e884e7a261f7c5e68cbdd7d7f80c8aa83350
mischelll <mishel.ivanovv@gmail.com>
2022-06-24 22:36:16 +0300

    modify GroupList - add delete

--- frontend/crud-app/src/components/GroupList.js
+++ frontend/crud-app/src/components/GroupList.js
@@ -12,22 +12,38 @@ export default function GroupList() {
     useEffect(() => {
         setIsLoading(true);
 
-         getAllGroups().then(data => {
+        getAllGroups().then(data => {
             setGroups(data);
             setIsLoading(false);
         })
 
     }, []);
 
+
+    const removeById = (id) => {
+        deleteGroupById(id)
+        .then(() => {
+            let updatedGroups = [...groups].filter(i => i.id !== id);
+            setGroups(updatedGroups);
+        });
+    }
+
     const groupList = groups.map(group => {
         const address = `${group.address || ''} ${group.city || ''} ${group.stateOrProvince || ''}`;
         return <tr key={group.id}>
             <td style={{ whiteSpace: 'nowrap' }}>{group.name}</td>
             <td>{address}</td>
+            <td>{group.events.map(event => {
+                return <div key={event.id}>{new Intl.DateTimeFormat('en-US', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: '2-digit'
+                }).format(new Date(event.date))}: {event.title}</div>
+            })}</td>
             <td>
                 <ButtonGroup>
                     <Button size="sm" color="primary" tag={Link} to={"/groups/" + group.id}>Edit</Button>
-                    {/* <Button size="sm" color="danger" onClick={() => remove(group.id)}>Delete</Button> */}
+                    <Button size="sm" color="danger" onClick={() => removeById(group.id)}>Delete</Button>
                 </ButtonGroup>
             </td>
         </tr>
@@ -39,25 +55,25 @@ export default function GroupList() {
 
     return (
         <>
-           <Container fluid>
-        <div className="float-end">
-          <Button color="success" tag={Link} to="/groups/new">Add Group</Button>
-        </div>
-        <h3>My JUG Tour</h3>
-        <Table className="mt-4">
-          <thead>
-          <tr>
-            <th width="20%">Name</th>
-            <th width="20%">Location</th>
-            <th>Events</th>
-            <th width="10%">Actions</th>
-          </tr>
-          </thead>
-          <tbody>
-          {groupList}
-          </tbody>
-        </Table>
-      </Container>
+            <Container fluid>
+                <div className="float-end">
+                    <Button color="success" tag={Link} to="/groups/new">Add Group</Button>
+                </div>
+                <h3>My JUG Tour</h3>
+                <Table className="mt-4">
+                    <thead>
+                        <tr>
+                            <th width="20%">Name</th>
+                            <th width="20%">Location</th>
+                            <th>Events</th>
+                            <th width="10%">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {groupList}
+                    </tbody>
+                </Table>
+            </Container>
         </>
     )
 }
