9ce9ac8b044eb2871b685123d63fd234fb778a81
mischelll <mishel.ivanovv@gmail.com>
2022-06-24 22:36:36 +0300

    extend groupService

--- frontend/crud-app/src/services/groupService.js
+++ frontend/crud-app/src/services/groupService.js
@@ -1,20 +1,39 @@
-const apiURL = 'http://localhost:8080/api/v1'
+const apiURL = 'http://localhost:8080/api/v1/groups'
 
 export const getAllGroups = () => {
-    let url = `${apiURL}/groups`;
-    return fetch(url).then(res => res.json());
+    return fetch(apiURL).then(res => res.json());
 }
 
 export const getGroupById = (id) => {
-    let url = `${apiURL}/groups/${id}`;
-    return fetch(url).then(res => res.json());
+    return fetch(`${apiURL}/${id}`).then(res => res.json());
 }
 
 export const deleteGroupById = (id) => {
-    let url = `${apiURL}/groups/${id}`;
-    return fetch(url,
+    return fetch(`${apiURL}/${id}`,
         {
             method: 'DELETE'
-        })
-        .then(res => res.json());
+        });
+}
+
+export const createGroup = (body) => {
+    return fetch(apiURL, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+    }).then(res => res.json());
+}
+
+export const updateGroup = (id, body) => {
+    return fetch(`${apiURL}/${id}`, {
+        method: "PATCH",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+    })
+    .then(res => res.json());
 }
