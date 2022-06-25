4dae590db05dd187402ee35d5197bbe6e5fbe474
mischelll <mishel.ivanovv@gmail.com>
2022-06-25 15:44:26 +0300

    add security on be

---
+++ src/main/java/com/example/crud/config/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.example.crud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SimpleSavedRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .antMatchers("/**/*.{js,html,css}").permitAll()
+                        .antMatchers("/", "/api/user").permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .oauth2Login();
+        return http.build();
+    }
+
+    @Bean
+    public RequestCache refererRequestCache() {
+        return new HttpSessionRequestCache() {
+            @Override
+            public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
+                String referrer = request.getHeader("referer");
+                if (referrer != null) {
+                    request.getSession().setAttribute("SPRING_SECURITY_SAVED_REQUEST",
+                            new SimpleSavedRequest(referrer));
+                }
+            }
+        };
+    }
+}
